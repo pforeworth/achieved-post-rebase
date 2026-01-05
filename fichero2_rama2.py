0901f863c883311c8ab14d5345ddc0a7ac1ab8e9
Pablo <pdelafuente@foreworth.com>
2026-01-05 09:38:49 +0100

    cambio4 after revert rama1

--- fichero2_rama2.py
+++ fichero2_rama2.py
@@ -19,19 +19,23 @@ class TaskQueue:
         self.queue.append(task)
 
     def run(self) -> None:
-        print("\nEjecutando tareas:\n")
+        print("\nEjecutando tareas:\ntareas")
         while self.queue:
             task = self.queue.popleft()
             print(f"→ {task.name} ({task.duration}s)")
             time.sleep(task.duration)
-            print(f"✓ {task.name} completada\n")
+            print(f"✓ {task.name} completatareasdtareasa\n tareas")
+            print(f"✓ {task.name} completatareasdtareasa\n cambio")
+            print(f"✓ {task.name} completatareasdtareasa\n 4")
+            print(f"✓ {task.name} completatareasdtareasa\n rama1")
+            print(f"✓ {task.name} completatareasdtareasa\n after revert")
 
 
 def main() -> None:
     q = TaskQueue()
-    q.add(Task("Descargar datos", 1))
-    q.add(Task("Procesar datos", 2))
-    q.add(Task("Generar reporte", 1.5))
+    q.add(Task("Descargar datostareas", 1))
+    q.add(Task("Procesar datostartareaseas", 2))
+    q.add(Task("Generar reportetareas", 1.5))
     q.run()
 
 
