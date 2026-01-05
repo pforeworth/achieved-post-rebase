402da35041bf96e87268c74f3446c29fbba587af
Pablo <pdelafuente@foreworth.com>
2026-01-05 09:34:21 +0100

    cambio3 master

--- fichero1_master.py
+++ fichero1_master.py
@@ -38,6 +38,11 @@ def main() -> None:
         print(f"Hash: {h[:12]}...")
         for f in files:
             print(f"  {f}")
+            print('cambio 3 master')
+            print('cambio 3 master')
+            print('cambio 3 master')
+            print('cambio 3 master')
+            print('cambio 3 master')
         print()
 
 
