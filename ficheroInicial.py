0785575aefbb72d122fc45ebbd8dcd7e820d6d72
Pablo <pdelafuente@foreworth.com>
2026-01-05 09:09:23 +0100

    Commit Incial

---
+++ ficheroInicial.py
@@ -0,0 +1,10 @@
+# pip install requests
+import requests
+
+def fetch(url: str):
+    r = requests.get(url, timeout=5)
+    r.raise_for_status()
+    return r.json()
+
+if __name__ == "__main__":
+    print(fetch("https://api.github.com"))
